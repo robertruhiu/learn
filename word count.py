b689d66126420d38fdb4e6b5f13804fcb398eabc
robert ruhiu <robertruhiu@gmail.com>
2017-09-11 15:20:28 +0000

    attribute names

--- word count.py
+++ word count.py
@@ -1,8 +1,8 @@
-text=("the quick brown fox jump over the lazy dog")
+text=("the quick brown fox jumped over the lazy dog")
 textd={}                    #initiate an empty dictionary
 for word in text.split(): #split each word in the text
     if word in textd.keys():
-        textd[word]+=1
+        textd[word]+=1      #incrimental counter
     else:
         textd[word]=1
-    print(textd)
+print(textd)
